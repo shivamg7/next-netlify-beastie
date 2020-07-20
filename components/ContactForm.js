e1f16341cad6d2e06500706f4f938c8a48cd493c
shivamgupta <shivam@soroco.com>
2020-07-21 00:35:51 +0530

    fix form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,5 +1,6 @@
 export default function ContactForm() {
     return <form name="contact" action="/success" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact"/>
         <p>
             <label htmlFor="yourEmail">Email !!</label>
             <input type="email" id="yourEmail"/>
